cd2cf343db2e348380d44a28b75ba6f27f16b25f
TeresaGT <gracteresa6@gmail.com>
2026-09-23 15:10:26 -0400

    nuevo componente pantalla

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
+import { Pantalla } from './pantalla/pantalla';
+import { Login } from './login/login';
+import { Portada } from './portada/portada';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Pantalla, Login, Portada],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
