012444782ee28323ace6df0b1cda2a6d9b064a05
TeresaGT <gracteresa6@gmail.com>
2026-09-23 16:24:00 -0400

    creacion de componentes para usuarios

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,13 @@ import { initFlowbite } from 'flowbite';
 import { Pantalla } from './pantalla/pantalla';
 import { Login } from './login/login';
 import { Portada } from './portada/portada';
+import { Carrera } from './carrera/carrera';
+import { Estudiante } from './estudiante/estudiante';
+import { Empresa } from './empresa/empresa';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Pantalla, Login, Portada],
+  imports: [RouterOutlet, Pantalla, Login, Portada, Carrera, Estudiante, Empresa],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/estudiante/estudiante.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estudiante',
+  imports: [],
+  templateUrl: './estudiante.html',
+  styleUrl: './estudiante.css',
+})
+export class Estudiante {}
