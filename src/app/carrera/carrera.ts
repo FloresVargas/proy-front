89de665f413f4198810310139904fe6e6013799c
Diegoxyz <aries-d2github@hotmail.com>
2026-09-25 11:33:01 -0400

    primer diseno para carrera terminado

--- src/app/carrera/carrera.ts
+++ src/app/carrera/carrera.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-carrera',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './carrera.html',
   styleUrl: './carrera.css',
 })
-export class Carrera {}
+export class Carrera {
+
+  isSidebarOpen: boolean = false; // Empieza cerrado por defecto
+
+  mostrarMenuAside() {
+    this.isSidebarOpen = !this.isSidebarOpen; // Cambia entre true y false
+  }
+}
